7323e2ebc1f14251302efd74c6d1aefd6dcfe124
TuanHo10102003 <anhtuan6563@gmail.com>
2025-06-02 21:01:37 +0700

    Checkout Page

---
+++ src/app/(app)/(tenants)/tenants/[slug]/(checkout)/layout.tsx
@@ -0,0 +1,25 @@
+import { Footer } from "@/components/ui/tenants/footer";
+import { Navbar } from "@/components/ui/checkout/navbar";
+
+import React, { Suspense } from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  params: Promise<{ slug: string }>;
+}
+
+const Layout = async ({ children, params }: LayoutProps) => {
+  const { slug } = await params;
+
+  return (
+    <div className="min-h-screen bg-[#F4F4F0] flex flex-col">
+      <Navbar slug={slug} />
+      <div className="flex-1">
+        <div className="max-w-(--breakpoint-xl) mx-auto">{children}</div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
