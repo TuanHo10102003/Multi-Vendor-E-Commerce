5c2f6ab2773e5a55d880119f8353bc524a48d21b
TuanHo10102003 <anhtuan6563@gmail.com>
2025-06-18 22:34:11 +0700

    fix: resolved invalid cookies

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -1,23 +1,21 @@
-
-import { cookies as getCookies } from "next/headers"
+import { cookies as getCookies } from "next/headers";
 
 interface Props {
-    prefix: string,
-    value: string,
+  prefix: string;
+  value: string;
 }
 
-export const generateAuthCookie = async ({
-    prefix,
-    value
-}: Props) => {
+export const generateAuthCookie = async ({ prefix, value }: Props) => {
   const cookies = await getCookies();
   cookies.set({
     name: `${prefix}-token`,
     value: value,
     httpOnly: true,
     path: "/",
-    sameSite: "none",
-    domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-    secure: process.env.NODE_ENV === "production"
+    ...(process.env.NODE_ENV != "development" && {
+      sameSite: "none",
+      domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+      secure: true,
+    }),
   });
 };
