426a9a0b258863227a850727200b839d5ec07afd
TuanHo10102003 <anhtuan6563@gmail.com>
2025-06-20 23:23:55 +0700

    Fix Search Filter

--- src/components/ui/search-filters/search-input.tsx
+++ src/components/ui/search-filters/search-input.tsx
@@ -1,4 +1,3 @@
-"use client";
 
 import { Input } from "@/components/ui/input";
 import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
@@ -8,15 +7,16 @@ import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import { useProductFilters } from "@/modules/products/hooks/use-product-filters";
 
 interface Props {
   disabled?: boolean;
+  defaultValue?: string | undefined;
+  onChange?: (value: string) => void;
 }
 
-export const SearchInput = ({ disabled }: Props) => {
-  const [filters, setFilters] = useProductFilters();
-  const [searchValue, setSearchValue] = useState(filters.search)
+export const SearchInput = ({ disabled, defaultValue, onChange }: Props) => {
+
+  const [searchValue, setSearchValue] = useState(defaultValue || "");
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const trpc = useTRPC();
@@ -24,11 +24,11 @@ export const SearchInput = ({ disabled }: Props) => {
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      setFilters({ search: searchValue })
-    }, 500)
+      onChange?.(searchValue);
+    }, 500);
 
-    return () => clearTimeout(timeoutId)
-  }, [searchValue, setFilters])
+    return () => clearTimeout(timeoutId);
+  }, [searchValue, onChange]);
 
   return (
     <div className="flex items-center gap-2 w-full">
